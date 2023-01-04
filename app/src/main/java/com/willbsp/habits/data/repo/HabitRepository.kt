cde7226ab8e8e98f3193e15883fbfeba6a145ce7
willbsp <github-wills.12zz7@aleeas.com>
2023-01-04 22:04:08 +0000

    Create habit repository implementation

---
+++ app/src/main/java/com/willbsp/habits/data/repo/HabitRepository.kt
@@ -0,0 +1,16 @@
+package com.willbsp.habits.data.repo
+
+import com.willbsp.habits.data.Habit
+import kotlinx.coroutines.flow.Flow
+
+interface HabitRepository {
+
+    fun getAllHabitsStream(): Flow<List<Habit>>
+
+    suspend fun insertHabit(habit: Habit)
+
+    suspend fun deleteHabit(habit: Habit)
+
+    suspend fun updateHabit(habit: Habit)
+
+}
---
+++ app/src/main/java/com/willbsp/habits/data/repo/OfflineHabitsRepository.kt
@@ -0,0 +1,25 @@
+package com.willbsp.habits.data.repo
+
+import com.willbsp.habits.data.Habit
+import com.willbsp.habits.data.database.HabitDao
+import kotlinx.coroutines.flow.Flow
+
+class OfflineHabitsRepository(private val habitDao: HabitDao) : HabitRepository {
+
+    override fun getAllHabitsStream(): Flow<List<Habit>> {
+        return habitDao.getAllHabits()
+    }
+
+    override suspend fun insertHabit(habit: Habit) {
+        habitDao.insert(habit)
+    }
+
+    override suspend fun deleteHabit(habit: Habit) {
+        habitDao.delete(habit)
+    }
+
+    override suspend fun updateHabit(habit: Habit) {
+        habitDao.update(habit)
+    }
+
+}
