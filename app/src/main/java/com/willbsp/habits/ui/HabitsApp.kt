c2fbd57b6041a99f51d05ab7c9fac6b155c880da
willbsp <github-wills.12zz7@aleeas.com>
2023-01-05 22:09:01 +0000

    Add top bar to home screen

--- app/src/main/java/com/willbsp/habits/ui/HabitsApp.kt
+++ app/src/main/java/com/willbsp/habits/ui/HabitsApp.kt
@@ -1,11 +1,43 @@
 package com.willbsp.habits.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.willbsp.habits.R
 import com.willbsp.habits.ui.navigation.HabitsNavigationGraph
 
 @Composable
 fun HabitsApp(navController: NavHostController = rememberNavController()) {
     HabitsNavigationGraph(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HabitsAppTopBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {}
+) {
+    if (canNavigateBack) {
+        CenterAlignedTopAppBar(
+            title = { Text(text = title) },
+            modifier = modifier,
+            navigationIcon = {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.topbar_back)
+                    )
+                }
+            }
+        )
+    } else {
+        CenterAlignedTopAppBar(title = { Text(title) }, modifier = modifier)
+    }
 }
--- app/src/main/java/com/willbsp/habits/ui/home/HomeScreen.kt
+++ app/src/main/java/com/willbsp/habits/ui/home/HomeScreen.kt
@@ -8,9 +8,12 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.willbsp.habits.ui.HabitsAppTopBar
 import com.willbsp.habits.ui.theme.HabitsTheme
+import com.willbsp.habits.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,6 +23,12 @@ fun HomeScreen(
 ) {
 
     Scaffold(
+        topBar = {
+            HabitsAppTopBar(
+                title = stringResource(R.string.app_name),
+                canNavigateBack = false
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = navigateToAddHabit,
