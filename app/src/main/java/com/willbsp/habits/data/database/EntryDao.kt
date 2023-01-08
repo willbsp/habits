c774c86e9433d9c089e5cdb209b3c2617c070728
willbsp <github-wills.12zz7@aleeas.com>
2023-01-08 11:51:46 +0000

    Remove unused imports from EntryDao.kt

--- app/src/main/java/com/willbsp/habits/data/database/EntryDao.kt
+++ app/src/main/java/com/willbsp/habits/data/database/EntryDao.kt
@@ -1,13 +1,7 @@
 package com.willbsp.habits.data.database
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.willbsp.habits.data.Entry
-import com.willbsp.habits.data.Habit
 import kotlinx.coroutines.flow.Flow
 
 @Dao
