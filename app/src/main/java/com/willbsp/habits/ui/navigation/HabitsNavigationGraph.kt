2ee9ca4531765d90aa0e5e4292a039140cc348d2
willbsp <github-wills.12zz7@aleeas.com>
2023-01-09 10:34:30 +0000

    Create enum class HabitsNavigationDestination to simplify navigation route specification

--- app/src/main/java/com/willbsp/habits/ui/navigation/HabitsNavigationGraph.kt
+++ app/src/main/java/com/willbsp/habits/ui/navigation/HabitsNavigationGraph.kt
@@ -8,6 +8,11 @@ import androidx.navigation.compose.composable
 import com.willbsp.habits.ui.add.AddHabitScreen
 import com.willbsp.habits.ui.home.HomeScreen
 
+enum class HabitsNavigationDestination(val route: String) {
+    HOME(route = "home"),
+    ADD(route = "add")
+}
+
 @Composable
 fun HabitsNavigationGraph(
     navController: NavHostController,
@@ -18,12 +23,14 @@ fun HabitsNavigationGraph(
         startDestination = "home", // TODO
         modifier = modifier
     ) {
-        composable(route = "home") { // TODO make into an enum
+        composable(route = HabitsNavigationDestination.HOME.route) {
             HomeScreen(
-                navigateToAddHabit = { navController.navigate("addhabitscreen") }
+                navigateToAddHabit = {
+                    navController.navigate(HabitsNavigationDestination.ADD.route)
+                }
             )
         }
-        composable(route = "addhabitscreen") {
+        composable(route = HabitsNavigationDestination.ADD.route) {
             AddHabitScreen(
                 navigateUp = { navController.navigateUp() },
                 navigateBack = { navController.popBackStack() }
