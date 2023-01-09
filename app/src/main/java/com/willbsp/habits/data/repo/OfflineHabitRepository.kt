67f993cfd7d131cc7205dd8e724fdaeff60a3e15
willbsp <github-wills.12zz7@aleeas.com>
2023-01-09 12:17:42 +0000

    Add implementation for entries in OfflineHabitRepository.kt

--- app/src/main/java/com/willbsp/habits/data/repo/OfflineHabitRepository.kt
+++ app/src/main/java/com/willbsp/habits/data/repo/OfflineHabitRepository.kt
@@ -1,13 +1,27 @@
 package com.willbsp.habits.data.repo
 
+import com.willbsp.habits.data.database.EntryDao
 import com.willbsp.habits.data.database.HabitDao
+import com.willbsp.habits.data.model.Entry
 import com.willbsp.habits.data.model.Habit
+import com.willbsp.habits.data.model.HabitWithEntries
 import kotlinx.coroutines.flow.Flow
 
-class OfflineHabitRepository(private val habitDao: HabitDao) : HabitRepository {
+class OfflineHabitRepository(
+    private val habitDao: HabitDao,
+    private val entryDao: EntryDao
+) : HabitRepository {
 
     override fun getAllHabitsStream(): Flow<List<Habit>> {
         return habitDao.getAllHabits()
+    }
+
+    override fun getAllHabitsWithEntriesStream(): Flow<List<HabitWithEntries>> {
+        return habitDao.getHabitsWithEntries()
+    }
+
+    override suspend fun insertEntry(entry: Entry) {
+        entryDao.insert(entry)
     }
 
     override suspend fun insertHabit(habit: Habit) {
