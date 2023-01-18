a0949d5bc710505ab3d4c9c876647a93eac23c8c
willbsp <github-wills.12zz7@aleeas.com>
2023-01-18 16:33:48 +0000

    Create viewmodels using hiltviewmodel in composables

--- app/src/main/java/com/willbsp/habits/ui/add/AddHabitScreen.kt
+++ app/src/main/java/com/willbsp/habits/ui/add/AddHabitScreen.kt
@@ -14,10 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.willbsp.habits.R
 import com.willbsp.habits.data.model.HabitFrequency
-import com.willbsp.habits.di.AppViewModelProvider
 import com.willbsp.habits.ui.HabitsAppTopBar
 import com.willbsp.habits.ui.theme.HabitsTheme
 import kotlinx.coroutines.launch
@@ -25,11 +24,11 @@ import kotlinx.coroutines.launch
 @Composable
 fun AddHabitScreen(
     modifier: Modifier = Modifier,
-    viewModel: AddHabitViewModel = viewModel(factory = AppViewModelProvider.Factory),
     navigateUp: () -> Unit,
     navigateBack: () -> Unit
 ) {
 
+    val viewModel = hiltViewModel<AddHabitViewModel>()
     val coroutineScope = rememberCoroutineScope()
 
     AddHabit(
--- app/src/main/java/com/willbsp/habits/ui/home/HomeScreen.kt
+++ app/src/main/java/com/willbsp/habits/ui/home/HomeScreen.kt
@@ -15,9 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.willbsp.habits.R
-import com.willbsp.habits.di.AppViewModelProvider
 import com.willbsp.habits.ui.HabitsAppTopBar
 import com.willbsp.habits.ui.theme.HabitsTheme
 import com.willbsp.habits.ui.theme.Typography
@@ -26,10 +25,10 @@ import kotlinx.coroutines.launch
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    navigateToAddHabit: () -> Unit,
-    viewModel: HomeScreenViewModel = viewModel(factory = AppViewModelProvider.Factory)
+    navigateToAddHabit: () -> Unit
 ) {
 
+    val viewModel = hiltViewModel<HomeScreenViewModel>()
     val homeUiState by viewModel.homeUiState.collectAsState(HomeUiState())
     val coroutineScope = rememberCoroutineScope()
 
