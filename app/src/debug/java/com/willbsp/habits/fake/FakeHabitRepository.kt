63cb76ac362e5dc6ebb7722e8438537feaa10f96
willbsp <github-wills.12zz7@aleeas.com>
2023-01-19 11:54:35 +0000

    Move fake data sources into debug folder

--- app/src/debug/java/com/willbsp/habits/fake/FakeHabitRepository.kt
+++ app/src/debug/java/com/willbsp/habits/fake/FakeHabitRepository.kt
@@ -6,13 +6,16 @@ import com.willbsp.habits.data.repo.HabitRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class FakeOfflineHabitRepository : HabitRepository {
+class FakeHabitRepository @Inject constructor() : HabitRepository {
 
     private val habitsStream = MutableSharedFlow<List<Habit>>()
-    suspend fun emit() = habitsStream.emit(FakeDataSource.habitTable)
+    private suspend fun emit() = habitsStream.emit(FakeDataSource.habitTable)
 
-    override fun getAllHabitsStream(): Flow<List<Habit>> = habitsStream
+    override fun getAllHabitsStream(): Flow<List<Habit>> {
+        return habitsStream
+    }
 
     override fun entryExistsForDateStream(date: String, habitId: Int): Flow<Boolean> {
         return flow { emit(FakeDataSource.entryExists(date, habitId)) }
@@ -29,26 +32,32 @@ class FakeOfflineHabitRepository : HabitRepository {
 
     override suspend fun insertHabit(habit: Habit) {
         FakeDataSource.habitTable.add(habit)
+        emit()
     }
 
     override suspend fun insertEntry(entry: Entry) {
         FakeDataSource.entryTable.add(entry)
+        emit()
     }
 
     override suspend fun deleteHabit(habit: Habit) {
         FakeDataSource.habitTable.remove(habit)
+        emit()
     }
 
     override suspend fun deleteEntry(entry: Entry) {
         FakeDataSource.entryTable.remove(entry)
+        emit()
     }
 
     override suspend fun updateHabit(habit: Habit) {
         TODO("Not yet implemented")
+        emit()
     }
 
     override suspend fun updateEntry(entry: Entry) {
         TODO("Not yet implemented")
+        emit()
     }
 
 }
