492db8316a38f27f422371965afcc99c6fe0e303
willbsp <github-wills.12zz7@aleeas.com>
2023-01-19 11:51:05 +0000

    Inject viewModels via the navigation graph

--- app/src/main/java/com/willbsp/habits/ui/navigation/HabitsNavigationGraph.kt
+++ app/src/main/java/com/willbsp/habits/ui/navigation/HabitsNavigationGraph.kt
@@ -2,11 +2,14 @@ package com.willbsp.habits.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.willbsp.habits.ui.add.AddHabitScreen
+import com.willbsp.habits.ui.add.AddHabitViewModel
 import com.willbsp.habits.ui.home.HomeScreen
+import com.willbsp.habits.ui.home.HomeScreenViewModel
 
 enum class HabitsNavigationDestination(val route: String) {
     HOME(route = "home"),
@@ -24,14 +27,18 @@ fun HabitsNavigationGraph(
         modifier = modifier
     ) {
         composable(route = HabitsNavigationDestination.HOME.route) {
+            val viewModel = hiltViewModel<HomeScreenViewModel>()
             HomeScreen(
+                viewModel = viewModel,
                 navigateToAddHabit = {
                     navController.navigate(HabitsNavigationDestination.ADD.route)
                 }
             )
         }
         composable(route = HabitsNavigationDestination.ADD.route) {
+            val viewModel = hiltViewModel<AddHabitViewModel>()
             AddHabitScreen(
+                viewModel = viewModel,
                 navigateUp = {
                     navController.navigateUp()
                 },
