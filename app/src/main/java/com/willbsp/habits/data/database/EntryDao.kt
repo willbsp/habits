5e610874fc82976111441b9f3d47ec8f7dfbbad2
willbsp <github-wills.12zz7@aleeas.com>
2023-01-20 15:46:00 +0000

    Simplify entrydao

--- app/src/main/java/com/willbsp/habits/data/database/EntryDao.kt
+++ app/src/main/java/com/willbsp/habits/data/database/EntryDao.kt
@@ -2,13 +2,9 @@ package com.willbsp.habits.data.database
 
 import androidx.room.*
 import com.willbsp.habits.data.model.Entry
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EntryDao {
-
-    @Query("SELECT EXISTS(SELECT * FROM entries WHERE date = :date AND habit_id = :habitId)")
-    fun entryExistsForDate(date: String, habitId: Int): Flow<Boolean>
 
     @Query("SELECT * FROM entries WHERE date = :date AND habit_id = :habitId")
     suspend fun getEntryForDate(date: String, habitId: Int): Entry?
