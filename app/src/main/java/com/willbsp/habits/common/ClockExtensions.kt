5dff9d0901c88727058ddc9762b2bb577eabe338
willbsp <github-wills.12zz7@aleeas.com>
2023-01-23 18:56:09 +0000

    Add function to ClockExtensions

--- app/src/main/java/com/willbsp/habits/common/ClockExtensions.kt
+++ app/src/main/java/com/willbsp/habits/common/ClockExtensions.kt
@@ -6,7 +6,20 @@ import java.time.format.DateTimeFormatter
 
 fun Clock.getCurrentFormattedDate(): String {
 
-    val formatter = DateTimeFormatter.ofPattern(DATE_FORMAT) // TODO make constant
+    val formatter = DateTimeFormatter.ofPattern(DATE_FORMAT)
     return LocalDateTime.now(this).format(formatter)
+
+}
+
+fun Clock.getPreviousDaysFormattedDate(days: Int): List<String> {
+
+    val formatter = DateTimeFormatter.ofPattern(DATE_FORMAT)
+    val clock = this
+
+    return buildList {
+        repeat(days) { index ->
+            this.add(LocalDateTime.now(clock).minusDays(index.toLong()).format(formatter))
+        }
+    }
 
 }
