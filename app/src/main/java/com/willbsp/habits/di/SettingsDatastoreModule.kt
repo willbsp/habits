98bb9391d30ea6b2ed77b5d84aa70b3027674e1a
willbsp <github-wills.12zz7@aleeas.com>
2023-02-01 20:11:41 +0000

    Create Hilt module for datastore

---
+++ app/src/main/java/com/willbsp/habits/di/SettingsDatastoreModule.kt
@@ -0,0 +1,29 @@
+package com.willbsp.habits.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.willbsp.habits.common.SETTINGS_DATASTORE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class SettingsDatastoreModule {
+
+    val Context.datastore by preferencesDataStore(name = SETTINGS_DATASTORE_NAME)
+
+    @Singleton
+    @Provides
+    fun provideSettingsDatastore(
+        @ApplicationContext app: Context
+    ): DataStore<Preferences> {
+        return app.datastore
+    }
+
+}
