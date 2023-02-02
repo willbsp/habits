72b92fb60cdb20ced8bbeeef613b23b7a2780459
willbsp <github-wills.12zz7@aleeas.com>
2023-02-02 10:45:41 +0000

    Add list item to settings screen for updating streak preference

--- app/src/main/java/com/willbsp/habits/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/willbsp/habits/ui/screens/settings/SettingsScreen.kt
@@ -1,18 +1,19 @@
 package com.willbsp.habits.ui.screens.settings
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.willbsp.habits.R
 import com.willbsp.habits.ui.common.DefaultHabitsAppTopBar
 import com.willbsp.habits.ui.theme.HabitsTheme
+import com.willbsp.habits.ui.theme.Typography
+import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsScreen(
@@ -22,10 +23,15 @@ fun SettingsScreen(
 ) {
 
     val settingsUiState by viewModel.settingsUiState.collectAsState(SettingsUiState())
+    val coroutineScope = rememberCoroutineScope()
 
     Settings(
         modifier = modifier,
-        navigateUp = navigateUp
+        navigateUp = navigateUp,
+        onDisplayStreaksPressed = {
+            coroutineScope.launch { viewModel.updateSetting(it) }
+        },
+        settingsUiState = settingsUiState
     )
 
 }
@@ -34,7 +40,9 @@ fun SettingsScreen(
 @Composable
 private fun Settings(
     modifier: Modifier = Modifier,
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    onDisplayStreaksPressed: (Boolean) -> Unit,
+    settingsUiState: SettingsUiState
 ) {
 
     Scaffold(
@@ -47,10 +55,32 @@ private fun Settings(
         }
     ) { innerPadding ->
 
-        Box(
+        Column(
             modifier = modifier
                 .padding(innerPadding)
+                .fillMaxSize()
         ) {
+
+            ListItem(
+                modifier = Modifier.fillMaxWidth(),
+                headlineText = {
+                    Text(
+                        text = "Display Streaks",
+                        style = Typography.titleLarge
+                    )
+                },
+                supportingText = {
+                    Text(
+                        text = "Display streaks on home screen",
+                        style = Typography.bodyMedium
+                    )
+                },
+                trailingContent = {
+                    Switch(
+                        checked = settingsUiState.showStreaksOnHome,
+                        onCheckedChange = { onDisplayStreaksPressed(it) })
+                }
+            )
 
         }
 
@@ -61,6 +91,6 @@ private fun Settings(
 @Composable
 private fun SettingsScreenPreview() {
     HabitsTheme {
-        Settings {}
+        //Settings {}
     }
 }
