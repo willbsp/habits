9e522da64e63f74cd20a57c58551747a2892db0b
willbsp <github-wills.12zz7@aleeas.com>
2023-02-08 20:14:17 +0000

    Add vibration to HabitToggleButton

--- app/src/main/java/com/willbsp/habits/ui/common/HabitToggleButton.kt
+++ app/src/main/java/com/willbsp/habits/ui/common/HabitToggleButton.kt
@@ -1,5 +1,11 @@
 package com.willbsp.habits.ui.common
 
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.size
@@ -8,11 +14,14 @@ import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.willbsp.habits.R
 
+// TODO fix requiresapi for lower apis, different way of getting vibrator?
+@RequiresApi(Build.VERSION_CODES.S)
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HabitToggleButton(
@@ -21,10 +30,19 @@ fun HabitToggleButton(
     checked: Boolean
 ) {
 
+    val v: VibratorManager =
+        LocalContext.current.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+    val vibrator: Vibrator = v.defaultVibrator
+
     AnimatedContent(targetState = checked) {
         FilledIconToggleButton(
             modifier = modifier.size(40.dp),
-            onCheckedChange = onCheckedChange,
+            onCheckedChange = {
+                vibrator.vibrate(
+                    VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
+                )
+                onCheckedChange(it)
+            },
             checked = it
         ) {
             Icon(
