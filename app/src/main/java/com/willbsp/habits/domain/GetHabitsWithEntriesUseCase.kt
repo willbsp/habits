c1da835b57912a1b3291451d1149e81c63fb952e
willbsp <github-wills.12zz7@aleeas.com>
2023-02-24 13:42:54 +0000

    Add method to use case for passing a date

--- app/src/main/java/com/willbsp/habits/domain/GetHabitsWithEntriesUseCase.kt
+++ app/src/main/java/com/willbsp/habits/domain/GetHabitsWithEntriesUseCase.kt
@@ -4,6 +4,7 @@ import com.willbsp.habits.data.repo.EntryRepository
 import com.willbsp.habits.data.repo.HabitRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import java.time.LocalDate
 import javax.inject.Inject
 
 class GetHabitsWithEntriesUseCase @Inject constructor(
@@ -16,6 +17,17 @@ class GetHabitsWithEntriesUseCase @Inject constructor(
             .combine(entryRepository.getAllEntries()) { habitList, entryList ->
                 habitList.map { habit ->
                     HabitWithEntries(habit, entryList.filter { it.habitId == habit.id })
+                }
+            }
+    }
+
+    operator fun invoke(date: LocalDate): Flow<List<HabitWithEntries>> {
+        return habitRepository.getAllHabits()
+            .combine(entryRepository.getAllEntries()) { habitList, entryList ->
+                habitList.map { habit ->
+                    HabitWithEntries(
+                        habit,
+                        entryList.filter { it.habitId == habit.id && it.date == date })
                 }
             }
     }
