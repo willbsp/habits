2e1b2e25d1eec4e55b62250e4ebd55639a8ccc34
willbsp <github-wills.12zz7@aleeas.com>
2023-02-27 12:09:11 +0000

    Move getEntry to the IO dispatcher

--- app/src/main/java/com/willbsp/habits/data/repository/local/LocalEntryRepository.kt
+++ app/src/main/java/com/willbsp/habits/data/repository/local/LocalEntryRepository.kt
@@ -3,7 +3,9 @@ package com.willbsp.habits.data.repository.local
 import com.willbsp.habits.data.database.dao.EntryDao
 import com.willbsp.habits.data.model.Entry
 import com.willbsp.habits.data.repository.EntryRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -19,9 +21,10 @@ class LocalEntryRepository @Inject constructor(
         return entryDao.getEntriesForHabit(habitId)
     }
 
-    override suspend fun getEntry(date: LocalDate, habitId: Int): Entry? {
-        return entryDao.getEntryForDate(habitId, date)
-    }
+    override suspend fun getEntry(date: LocalDate, habitId: Int): Entry? =
+        withContext(Dispatchers.IO) {
+            return@withContext entryDao.getEntryForDate(habitId, date)
+        }
 
     override suspend fun getOldestEntry(habitId: Int): Entry? {
         return entryDao.getOldestEntry(habitId)
