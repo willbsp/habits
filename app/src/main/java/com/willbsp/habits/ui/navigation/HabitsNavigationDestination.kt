7ff0866aaed617032aff9486a0d877626c48e730
willbsp <github-wills.12zz7@aleeas.com>
2023-02-27 12:09:36 +0000

    Add detail to HabitsNavigationDestination

--- app/src/main/java/com/willbsp/habits/ui/navigation/HabitsNavigationDestination.kt
+++ app/src/main/java/com/willbsp/habits/ui/navigation/HabitsNavigationDestination.kt
@@ -3,6 +3,7 @@ package com.willbsp.habits.ui.navigation
 enum class HabitsNavigationDestination(val route: String) {
     HOME(route = "home"),
     ADD(route = "add"),
+    DETAIL(route = "detail/"),
     EDIT(route = "edit/"),
     LOGBOOK(route = "logbook"),
     SETTINGS(route = "settings")
