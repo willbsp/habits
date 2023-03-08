3bf25fb61ecea00bcd2346bc3b2584497174fcff
willbsp <github-wills.12zz7@aleeas.com>
2023-03-08 14:39:38 +0000

    Add default parameter to detail ui state

--- app/src/main/java/com/willbsp/habits/ui/screens/detail/DetailUiState.kt
+++ app/src/main/java/com/willbsp/habits/ui/screens/detail/DetailUiState.kt
@@ -2,7 +2,7 @@ package com.willbsp.habits.ui.screens.detail
 
 data class DetailUiState(
     val habitId: Int,
-    val habitName: String,
+    val habitName: String = "",
     val streak: Int = 0,
     val score: Int = 0,
 )
