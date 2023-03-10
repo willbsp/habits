95c84d1fc261e0161b778c664e3f7236668135f3
willbsp <github-wills.12zz7@aleeas.com>
2023-03-10 07:34:10 +0000

    Fix animation issue with navigation due to background not being set to theme colour

--- app/src/main/java/com/willbsp/habits/MainActivity.kt
+++ app/src/main/java/com/willbsp/habits/MainActivity.kt
@@ -3,6 +3,8 @@ package com.willbsp.habits
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import com.willbsp.habits.ui.HabitsApp
 import com.willbsp.habits.ui.theme.HabitsTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +15,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HabitsTheme {
-                HabitsApp()
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    HabitsApp()
+                }
             }
         }
 
