38330bba3f50962b5dc405e059be9811666172bc
willbsp <github-wills.12zz7@aleeas.com>
2023-03-10 16:46:14 +0000

    Create FakeEntryRepository

---
+++ app/src/test/java/com/willbsp/habits/fake/FakeEntryRepository.kt
@@ -0,0 +1,45 @@
+package com.willbsp.habits.fake
+
+import com.willbsp.habits.data.model.Entry
+import com.willbsp.habits.data.repository.EntryRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import java.time.LocalDate
+
+class FakeEntryRepository : EntryRepository {
+
+    private val entries = mutableListOf<Entry>()
+
+    private var observableEntries = MutableStateFlow<List<Entry>>(listOf())
+    private suspend fun emit() {
+        observableEntries.emit(entries)
+    }
+
+    override fun getEntries(): Flow<List<Entry>> {
+        return observableEntries
+    }
+
+    override fun getEntries(habitId: Int): Flow<List<Entry>> {
+        return observableEntries.map { entry -> entry.filter { it.habitId == habitId } }
+    }
+
+    override suspend fun getEntry(date: LocalDate, habitId: Int): Entry? {
+        return entries.find { it.date == date && it.habitId == habitId }
+    }
+
+    override suspend fun getOldestEntry(habitId: Int): Entry? {
+        return entries.filter { it.habitId == habitId }.sortedBy { it.date }.findLast { true }
+    }
+
+    override suspend fun toggleEntry(habitId: Int, date: LocalDate) {
+        val entry = entries.find { it.habitId == habitId && it.date == date }
+        if (entry == null) {
+            entries.add(Entry(entries.lastIndex + 1, habitId, date))
+        } else {
+            entries.remove(entry)
+        }
+        emit()
+    }
+
+}
