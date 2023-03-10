19257f571a648e461e38b8509adc486fe98955d2
willbsp <github-wills.12zz7@aleeas.com>
2023-03-10 16:56:14 +0000

    Refactor FakeHabitRepository

--- app/src/test/java/com/willbsp/habits/fake/FakeHabitRepository.kt
+++ app/src/test/java/com/willbsp/habits/fake/FakeHabitRepository.kt
@@ -1,81 +1,43 @@
 package com.willbsp.habits.fake
 
-import com.willbsp.habits.data.model.Entry
 import com.willbsp.habits.data.model.Habit
-import com.willbsp.habits.data.model.HabitWithEntries
 import com.willbsp.habits.data.repository.HabitRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import java.time.LocalDate
+import kotlinx.coroutines.flow.map
 
 class FakeHabitRepository : HabitRepository {
 
-    val habits = mutableListOf<Habit>()
-    private val entries = mutableListOf<Entry>()
+    private val habits = mutableListOf<Habit>()
+    private var observableHabits = MutableStateFlow<List<Habit>>(listOf())
+    private suspend fun emit() = observableHabits.emit(habits)
 
-    private var observableHabitEntries = MutableStateFlow<List<HabitWithEntries>>(listOf())
-    private var dates: List<LocalDate> = listOf()
-
-    override fun getAllHabitsWithEntriesForDates(dates: List<LocalDate>): Flow<List<HabitWithEntries>> {
-        this.dates = dates
-        return observableHabitEntries
+    override fun getAllHabitsStream(): Flow<List<Habit>> {
+        return observableHabits
     }
 
-    override suspend fun getHabitById(habitId: Int): Habit {
-        return habits.first {
-            it.id == habitId
-        }
+    override fun getHabitStream(habitId: Int): Flow<Habit?> {
+        return observableHabits.map { it.find { habit -> habit.id == habitId } }
     }
 
-    override suspend fun getEntryForDate(date: LocalDate, habitId: Int): Entry? {
-        return try {
-            entries.first {
-                it.habitId == habitId && it.date == date
-            }
-        } catch (e: NoSuchElementException) {
-            null
-        }
+    override suspend fun getHabit(habitId: Int): Habit? {
+        return habits.find { it.id == habitId }
     }
 
     override suspend fun addHabit(habit: Habit) {
-        habits.add(habit)
+        habits.add(Habit(id = habits.lastIndex + 1, name = habit.name, frequency = habit.frequency))
         emit()
     }
 
     override suspend fun updateHabit(habit: Habit) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun toggleEntry(habitId: Int, date: LocalDate) {
-        val entry: Entry? = getEntryForDate(habitId = habitId, date = date)
-        val index = entries.lastIndex
-        if (entry == null) entries.add(Entry(id = index, habitId = habitId, date = date))
-        else entries.remove(entry)
+        val index = habits.indexOfFirst { it.id == habit.id }
+        if (index != -1) habits[index] = habit
         emit()
     }
 
     override suspend fun deleteHabit(habitId: Int) {
-        habits.remove(getHabitById(habitId))
+        habits.removeAll { it.id == habitId }
         emit()
-    }
-
-    /**
-     * Call after every change to the data to imitate flows emitting when database is changed
-     */
-    private suspend fun emit() {
-        observableHabitEntries.emit(getHabitEntriesForDates(dates))
-    }
-
-    /**
-     * Map the list of habits and list of entries to habit entries
-     */
-    private fun getHabitEntriesForDates(dates: List<LocalDate>): List<HabitWithEntries> {
-        return habits.map { habit ->
-            val entriesForHabit = entries.filter { entry ->
-                entry.habitId == habit.id && dates.any { entry.date == it }
-            }
-            HabitWithEntries(habit, entriesForHabit.requireNoNulls())
-        }
     }
 
 }
