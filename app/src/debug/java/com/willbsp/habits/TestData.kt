cabe44630e1a2de1462df76be89fd37ed52f24aa
willbsp <github-wills.12zz7@aleeas.com>
2023-03-10 17:46:31 +0000

    Update test data to add a list of entries

--- app/src/debug/java/com/willbsp/habits/TestData.kt
+++ app/src/debug/java/com/willbsp/habits/TestData.kt
@@ -13,4 +13,15 @@ object TestData {
     val entry1: Entry = Entry(1, 1, LocalDate.parse("2023-01-13"))
     val entry2: Entry = Entry(2, 2, LocalDate.parse("2023-04-15"))
 
+    // consecutive entries for testing streak calculation
+    val entryList: List<Entry> = listOf(
+        entry1,
+        entry2,
+        Entry(3, 2, LocalDate.parse("2023-07-15")),
+        Entry(4, 2, LocalDate.parse("2023-07-16")),
+        Entry(5, 2, LocalDate.parse("2023-07-17")),
+        Entry(6, 2, LocalDate.parse("2023-07-18")),
+        Entry(7, 2, LocalDate.parse("2023-07-19"))
+    )
+
 }
