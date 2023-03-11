6af7dffbbe48670e6b1882017f49c4de8a066320
willbsp <github-wills.12zz7@aleeas.com>
2023-03-11 13:17:53 +0000

    Fix TestData

--- app/src/debug/java/com/willbsp/habits/TestData.kt
+++ app/src/debug/java/com/willbsp/habits/TestData.kt
@@ -18,17 +18,17 @@ object TestData {
     // streak (from 07-20): 5
     // score: ~37%
     val entryList: List<Entry> = listOf(
-        Entry(7, 3, LocalDate.parse("2023-02-11")),
-        Entry(8, 3, LocalDate.parse("2023-02-13")),
-        Entry(9, 3, LocalDate.parse("2023-02-23")),
-        Entry(10, 3, LocalDate.parse("2023-02-27")),
-        Entry(11, 3, LocalDate.parse("2023-03-01")),
-        Entry(12, 3, LocalDate.parse("2023-03-03")),
-        Entry(13, 3, LocalDate.parse("2023-03-05")),
-        Entry(14, 3, LocalDate.parse("2023-03-06")),
-        Entry(15, 3, LocalDate.parse("2023-03-07")),
-        Entry(16, 3, LocalDate.parse("2023-03-08")),
-        Entry(17, 3, LocalDate.parse("2023-03-09"))
+        Entry(7, 2, LocalDate.parse("2023-02-11")),
+        Entry(8, 2, LocalDate.parse("2023-02-13")),
+        Entry(9, 2, LocalDate.parse("2023-02-23")),
+        Entry(10, 2, LocalDate.parse("2023-02-27")),
+        Entry(11, 2, LocalDate.parse("2023-03-01")),
+        Entry(12, 2, LocalDate.parse("2023-03-03")),
+        Entry(13, 2, LocalDate.parse("2023-03-05")),
+        Entry(14, 2, LocalDate.parse("2023-03-06")),
+        Entry(15, 2, LocalDate.parse("2023-03-07")),
+        Entry(16, 2, LocalDate.parse("2023-03-08")),
+        Entry(17, 2, LocalDate.parse("2023-03-09"))
     )
 
 }
