9659e01c50c7b583b6493f024e57f738679cbda9
willbsp <github-wills.12zz7@aleeas.com>
2023-03-12 13:12:13 +0000

    Refactor FakeHabitWithEntriesRepository to be able to take a HabitWithEntriesDao

--- app/src/test/java/com/willbsp/habits/fake/FakeHabitWithEntriesRepository.kt
+++ app/src/test/java/com/willbsp/habits/fake/FakeHabitWithEntriesRepository.kt
@@ -1,5 +1,6 @@
 package com.willbsp.habits.fake
 
+import com.willbsp.habits.data.database.dao.HabitWithEntriesDao
 import com.willbsp.habits.data.model.HabitWithEntries
 import com.willbsp.habits.data.repository.EntryRepository
 import com.willbsp.habits.data.repository.HabitRepository
@@ -7,15 +8,26 @@ import com.willbsp.habits.data.repository.HabitWithEntriesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 
-class FakeHabitWithEntriesRepository(
-    private val habitRepository: HabitRepository,
-    private val entryRepository: EntryRepository
-) : HabitWithEntriesRepository {
+class FakeHabitWithEntriesRepository : HabitWithEntriesRepository {
+
+    private var habitRepository: HabitRepository? = null
+    private var entryRepository: EntryRepository? = null
+    private var habitWithEntriesDao: HabitWithEntriesDao? = null
+
+    constructor(habitRepository: HabitRepository, entryRepository: EntryRepository) {
+        this.habitRepository = habitRepository
+        this.entryRepository = entryRepository
+    }
+
+    constructor(habitWithEntriesDao: HabitWithEntriesDao?) {
+        this.habitWithEntriesDao = habitWithEntriesDao
+    }
 
     override fun getHabitsWithEntries(): Flow<List<HabitWithEntries>> {
+        if (habitWithEntriesDao != null) return habitWithEntriesDao!!.getHabitsWithEntries()
         return combine(
-            habitRepository.getAllHabitsStream(),
-            entryRepository.getAllEntriesStream()
+            habitRepository!!.getAllHabitsStream(),
+            entryRepository!!.getAllEntriesStream()
         ) { habitList, entryList ->
             habitList.map { habit ->
                 HabitWithEntries(habit, entryList.filter { it.habitId == habit.id })
