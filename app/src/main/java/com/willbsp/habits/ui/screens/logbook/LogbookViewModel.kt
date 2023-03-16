13d22ebe81feb2f1ad2b2dd8f9623fdfbf0c1cd5
willbsp <github-wills.12zz7@aleeas.com>
2023-03-16 20:16:53 +0000

    Refactor LogbookViewModel for new ui state

--- app/src/main/java/com/willbsp/habits/ui/screens/logbook/LogbookViewModel.kt
+++ app/src/main/java/com/willbsp/habits/ui/screens/logbook/LogbookViewModel.kt
@@ -2,49 +2,44 @@ package com.willbsp.habits.ui.screens.logbook
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.willbsp.habits.data.model.HabitWithEntries
 import com.willbsp.habits.data.repository.EntryRepository
-import com.willbsp.habits.data.repository.HabitWithEntriesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.time.Clock
 import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
 class LogbookViewModel @Inject constructor(
-    private val habitRepository: HabitWithEntriesRepository,
-    private val entryRepository: EntryRepository,
-    clock: Clock
+    private val entryRepository: EntryRepository
 ) : ViewModel() {
 
+    private var habitId: Int? = null
 
-    private val _uiState = MutableStateFlow(LogbookUiState())
+    private val _uiState: MutableStateFlow<LogbookUiState> =
+        MutableStateFlow(LogbookUiState.NoSelection)
     val uiState: StateFlow<LogbookUiState> = _uiState
 
     init {
-        setSelectedDate(LocalDate.now(clock))
+        setSelectedHabit(1)
     }
 
-    fun setSelectedDate(date: LocalDate) {
+    fun setSelectedHabit(habitId: Int) {
+        this.habitId = habitId
         viewModelScope.launch {
-            habitRepository.getHabitsWithEntries().collect { list ->
-                _uiState.value = LogbookUiState(
-                    list.map { habitWithEntries -> habitWithEntries.toLogbookHabitUiState(date) }
-                )
+            entryRepository.getAllEntriesStream(habitId).collect { list ->
+                _uiState.value = LogbookUiState.Dates(list.map { it.date })
             }
         }
     }
 
-    fun toggleEntry(habitId: Int, date: LocalDate) {
-        viewModelScope.launch {
-            entryRepository.toggleEntry(habitId, date)
+    fun toggleEntry(date: LocalDate) {
+        if (habitId != null) {
+            viewModelScope.launch {
+                entryRepository.toggleEntry(habitId!!, date)
+            }
         }
     }
-
-    private fun HabitWithEntries.toLogbookHabitUiState(date: LocalDate): LogbookHabitUiState =
-        LogbookHabitUiState(habit.id, habit.name, entries.any { it.date == date })
 
 }
