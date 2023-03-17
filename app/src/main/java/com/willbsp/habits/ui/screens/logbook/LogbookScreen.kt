0c704b6f1c5d5c55a46c0423110f9b50a96ea0bc
willbsp <github-wills.12zz7@aleeas.com>
2023-03-17 11:09:15 +0000

    Add habit picker to the bottom of logbook screen

--- app/src/main/java/com/willbsp/habits/ui/screens/logbook/LogbookScreen.kt
+++ app/src/main/java/com/willbsp/habits/ui/screens/logbook/LogbookScreen.kt
@@ -1,11 +1,15 @@
 package com.willbsp.habits.ui.screens.logbook
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.willbsp.habits.R
@@ -24,8 +28,9 @@ fun LogbookScreen(
 
     Logbook(
         modifier = modifier,
-        logbookCalendarUiState = logbookUiState,
+        logbookUiState = logbookUiState,
         completedOnClick = { date -> viewModel.toggleEntry(date) },
+        habitOnClick = { habitId -> viewModel.setSelectedHabit(habitId) },
         navigateUp = navigateUp
     )
 
@@ -36,8 +41,9 @@ fun LogbookScreen(
 private fun Logbook(
     modifier: Modifier = Modifier,
     navigateUp: () -> Unit,
-    logbookCalendarUiState: LogbookCalendarUiState,
-    completedOnClick: (LocalDate) -> Unit
+    logbookUiState: LogbookUiState,
+    completedOnClick: (LocalDate) -> Unit,
+    habitOnClick: (Int) -> Unit
 ) {
 
     Scaffold(
@@ -50,23 +56,52 @@ private fun Logbook(
         },
     ) { innerPadding ->
 
-        when (logbookCalendarUiState) {
+        when (logbookUiState) {
 
-            is LogbookCalendarUiState.SelectedHabit -> {
+            is LogbookUiState.SelectedHabit -> {
 
-                LogbookDatePicker(
-                    modifier = Modifier
+                Column(
+                    modifier = modifier
                         .padding(innerPadding)
                         .fillMaxSize(),
-                    dates = logbookCalendarUiState,
-                    selectedHabitId = 3,
-                    dateOnClick = completedOnClick
-                )
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+
+
+                    LogbookDatePicker(
+                        modifier = Modifier.weight(1f, false),
+                        dates = logbookUiState.selectedHabitDates,
+                        selectedHabitId = 3,
+                        dateOnClick = completedOnClick
+                    )
+
+                    Divider()
+
+                    LazyRow(
+                        modifier = Modifier
+                            .height(60.dp)
+                            .padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        items(items = logbookUiState.habits, key = { it.id }) {
+                            FilterChip(
+                                modifier = Modifier
+                                    .padding(end = 8.dp),
+                                selected = it.id == logbookUiState.selectedHabitId,
+                                onClick = { habitOnClick(it.id) },
+                                label = { Text(text = it.name) })
+                        }
+                    }
+
+                }
 
             }
 
             else -> { // TODO
-                Text("this text should not be here!")
+                Text(
+                    modifier = modifier.padding(innerPadding),
+                    text = "this text should not be here!"
+                )
             }
 
         }
@@ -102,8 +137,15 @@ private fun Logbook(
 fun LogbookPreview() {
     Logbook(
         navigateUp = {},
-        logbookCalendarUiState = LogbookCalendarUiState.NoSelection,
-        //onSelectedHabitChange = {},
-        completedOnClick = { }
+        logbookUiState = LogbookUiState.SelectedHabit(
+            listOf(
+                LogbookUiState.Habit(id = 0, name = "Running"),
+                LogbookUiState.Habit(id = 1, name = "Flashcards"),
+                LogbookUiState.Habit(id = 2, name = "Reading"),
+                LogbookUiState.Habit(id = 3, name = "Meditation"),
+            ), 0, listOf()
+        ),
+        completedOnClick = { },
+        habitOnClick = { }
     )
 }
