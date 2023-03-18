b840fd8541854c7b9c2921800135e56a5bdafb4f
willbsp <github-wills.12zz7@aleeas.com>
2023-03-18 15:21:24 +0000

    Add method to return habit from modify habit ui state

--- app/src/main/java/com/willbsp/habits/ui/common/ModifyHabitUiState.kt
+++ app/src/main/java/com/willbsp/habits/ui/common/ModifyHabitUiState.kt
@@ -1,5 +1,6 @@
 package com.willbsp.habits.ui.common
 
+import com.willbsp.habits.data.model.Habit
 import com.willbsp.habits.data.model.HabitFrequency
 
 data class ModifyHabitUiState(
@@ -7,3 +8,8 @@ data class ModifyHabitUiState(
     val nameIsInvalid: Boolean = false,
     val frequency: HabitFrequency = HabitFrequency.DAILY,
 )
+
+fun ModifyHabitUiState.toHabit(id: Int? = null): Habit {
+    return if (id != null) Habit(id = id, name = this.name, frequency = this.frequency)
+    else Habit(name = this.name, frequency = this.frequency)
+}
