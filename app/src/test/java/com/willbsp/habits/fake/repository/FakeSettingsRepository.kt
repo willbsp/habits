f1c3a59d8affab22cd3f83d594c8f815c96ed5fc
willbsp <github-wills.12zz7@aleeas.com>
2023-03-19 13:00:49 +0000

    Update FakeSettingsRepository

--- app/src/test/java/com/willbsp/habits/fake/repository/FakeSettingsRepository.kt
+++ app/src/test/java/com/willbsp/habits/fake/repository/FakeSettingsRepository.kt
@@ -5,21 +5,44 @@ import com.willbsp.habits.data.repository.SettingsRepository
 import com.willbsp.habits.data.repository.SettingsRepository.SettingsKey.SHOW_COMPLETED_SUBTITLE
 import com.willbsp.habits.data.repository.SettingsRepository.SettingsKey.SHOW_STREAKS_ON_HOME
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
 class FakeSettingsRepository : SettingsRepository {
 
-    var prefs = mutableMapOf<Preferences.Key<*>, Any>()
+    val preferences = mutableMapOf<Preferences.Key<*>, Any>()
+    private val observablePreferences =
+        MutableSharedFlow<Map<Preferences.Key<*>, Any>>(1)
 
-    override val preferences: Flow<Map<Preferences.Key<*>, Any>>
-        get() = flow { prefs }
+    init {
+        runBlocking {
+            emit()
+        }
+    }
+
+    suspend fun emit() {
+        observablePreferences.emit(preferences)
+    }
+
+    override fun getSettingsMap(): Flow<Map<Preferences.Key<*>, Any>> = observablePreferences
+
+    override fun getStreakPreference(): Flow<Boolean> =
+        observablePreferences.map {
+            it[SHOW_STREAKS_ON_HOME] as Boolean? ?: true
+        }
+
+    override fun getSubtitlePreference(): Flow<Boolean> =
+        observablePreferences.map { it[SHOW_COMPLETED_SUBTITLE] as Boolean? ?: true }
 
     override suspend fun saveStreaksPreference(showStreaksOnHome: Boolean) {
-        prefs[SHOW_STREAKS_ON_HOME] = showStreaksOnHome
+        preferences[SHOW_STREAKS_ON_HOME] = showStreaksOnHome
+        emit()
     }
 
     override suspend fun saveSubtitlePreference(showCompletedSubtitle: Boolean) {
-        prefs[SHOW_COMPLETED_SUBTITLE] = showCompletedSubtitle
+        preferences[SHOW_COMPLETED_SUBTITLE] = showCompletedSubtitle
+        emit()
     }
 
 }
