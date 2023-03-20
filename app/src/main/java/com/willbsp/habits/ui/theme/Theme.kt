2ba6ced85f104486a0b8a5dceb1f7e61b3031602
willbsp <github-wills.12zz7@aleeas.com>
2023-03-20 11:25:59 +0000

    Make status bar background colour, and do the same when gesture navigation is enabled for the navigation bar

--- app/src/main/java/com/willbsp/habits/ui/theme/Theme.kt
+++ app/src/main/java/com/willbsp/habits/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.willbsp.habits.ui.theme
 
 import android.app.Activity
+import android.content.Context
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +15,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -49,14 +51,20 @@ fun HabitsTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
+    val context = LocalContext.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
+            if (isEdgeToEdgeEnabled(context) == 2) {
+                (view.context as Activity).window.navigationBarColor =
+                    colorScheme.background.toArgb()
+            }
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
         }
     }
 
@@ -65,4 +73,12 @@ fun HabitsTheme(
         typography = Typography,
         content = content
     )
+}
+
+fun isEdgeToEdgeEnabled(context: Context): Int {
+    val resources = context.resources
+    val resourceId = resources.getIdentifier("config_navBarInteractionMode", "integer", "android")
+    return if (resourceId > 0) {
+        resources.getInteger(resourceId)
+    } else 0
 }
