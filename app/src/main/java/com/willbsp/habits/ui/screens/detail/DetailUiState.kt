2c33cc9d43ee9699df8f5366f1bd74f95f8db46b
willbsp <github-wills.12zz7@aleeas.com>
2023-03-26 22:35:25 +0100

    Add members to DetailUiState

--- app/src/main/java/com/willbsp/habits/ui/screens/detail/DetailUiState.kt
+++ app/src/main/java/com/willbsp/habits/ui/screens/detail/DetailUiState.kt
@@ -1,8 +1,13 @@
 package com.willbsp.habits.ui.screens.detail
 
+import java.time.LocalDate
+
 data class DetailUiState(
     val habitId: Int,
     val habitName: String = "",
     val streak: Int = 0,
+    val longestStreak: Int = 0,
+    val started: LocalDate = LocalDate.now(),
+    val totalDays: Int = 0,
     val score: Int = 0,
 )
