7bea80a24fc0f692e2bcb316771785303a35f683
willbsp <github-wills.12zz7@aleeas.com>
2023-04-05 17:57:17 +0100

    Fix habit form so that drop down only goes up to 6

--- app/src/main/java/com/willbsp/habits/ui/common/HabitForm.kt
+++ app/src/main/java/com/willbsp/habits/ui/common/HabitForm.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.willbsp.habits.R
@@ -102,7 +100,7 @@ private fun HabitFrequencyDropdown(
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        val repeatOptions = 1..7
+        val repeatOptions = 1..6
         var repeatExpanded by remember { mutableStateOf(false) }
         var selectedRepeat by remember { mutableStateOf(uiState.repeat) }
 
