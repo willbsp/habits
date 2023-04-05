3b1d57759abad0ea0329db4249ec980e31a608d7
willbsp <github-wills.12zz7@aleeas.com>
2023-04-05 11:51:30 +0100

    Refactor to depend on GetVirtualEntriesUseCase

--- app/src/main/java/com/willbsp/habits/domain/usecase/GetHabitsWithVirtualEntriesUseCase.kt
+++ app/src/main/java/com/willbsp/habits/domain/usecase/GetHabitsWithVirtualEntriesUseCase.kt
@@ -1,15 +1,12 @@
 package com.willbsp.habits.domain.usecase
 
-import com.willbsp.habits.data.model.Entry
-import com.willbsp.habits.data.model.HabitFrequency
-import com.willbsp.habits.data.model.HabitWithEntries
-import com.willbsp.habits.data.repository.HabitWithEntriesRepository
+import com.willbsp.habits.data.repository.HabitRepository
 import com.willbsp.habits.domain.model.HabitWithVirtualEntries
-import com.willbsp.habits.domain.model.VirtualEntry
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
-import java.time.DayOfWeek
-import java.time.LocalDate
 import javax.inject.Inject
 
 /**
@@ -17,80 +14,24 @@ import javax.inject.Inject
  * Returns filled dates for weekly habits else normal dates for daily habits
  */
 class GetHabitsWithVirtualEntriesUseCase @Inject constructor(
-    val habitRepository: HabitWithEntriesRepository
+    val habitRepository: HabitRepository,
+    val getVirtualEntries: GetVirtualEntriesUseCase
 ) {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(): Flow<List<HabitWithVirtualEntries>> {
 
-        return habitRepository.getHabitsWithEntries().map invokeMap@{ habitsWithEntries ->
+        return habitRepository.getAllHabitsStream().flatMapLatest { habitList ->
 
-            if (habitsWithEntries.isEmpty())
-                return@invokeMap listOf<HabitWithVirtualEntries>()
-
-            return@invokeMap habitsWithEntries.map { habitWithEntries ->
-
-                val habit = habitWithEntries.habit
-                val entries = habitWithEntries.entries.sortedByDescending { it.date }
-
-                if (entries.isEmpty())
-                    return@map HabitWithVirtualEntries(habit, listOf())
-
-                when (habit.frequency) {
-
-                    HabitFrequency.WEEKLY -> {
-
-                        val virtualEntries = mutableListOf<VirtualEntry>()
-                        val completedWeeks = mutableListOf<LocalDate>()
-
-                        var week = entries.first().date.with(DayOfWeek.MONDAY)
-                        var count = 0
-
-                        entries.forEach { entry ->
-                            virtualEntries.add(entry.toVirtualEntry())
-                            val entryWeek = entry.date.with(DayOfWeek.MONDAY)
-                            if (entryWeek == week && !completedWeeks.contains(entryWeek)) count++
-                            else {
-                                week = entryWeek
-                                count = 1
-                            }
-                            if (count >= habit.repeat) completedWeeks.add(entryWeek)
-                        }
-
-                        completedWeeks.forEach { weekStart ->
-                            (0..6).forEach { offset ->
-                                val date = weekStart.plusDays(offset.toLong())
-                                if (!virtualEntries.any { it.date == date })
-                                    virtualEntries.add(VirtualEntry(null, habit.id, date))
-                            }
-                        }
-
-                        return@map HabitWithVirtualEntries(
-                            habit,
-                            virtualEntries.sortedByDescending { it.date }
-                        )
-                    }
-
-                    HabitFrequency.DAILY -> {
-                        habitWithEntries.toHabitWithVirtualEntries()
-                    }
-
+            val flows = habitList.map { habit ->
+                return@map getVirtualEntries(habit.id).map { virtualEntries ->
+                    HabitWithVirtualEntries(habit, virtualEntries)
                 }
-
             }
+            combine(flows) { it.toList() }
 
         }
 
     }
-
-    private fun HabitWithEntries.toHabitWithVirtualEntries(): HabitWithVirtualEntries {
-        return HabitWithVirtualEntries(
-            this.habit,
-            this.entries.map { it.toVirtualEntry() }.sortedByDescending { it.date })
-    }
-
-    private fun Entry.toVirtualEntry(): VirtualEntry {
-        return VirtualEntry(id = id, habitId = habitId, date = date)
-    }
-
 
 }
