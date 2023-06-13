dd0fdd944ede0058bd37e19cdd4c399c615aff8f
willbsp <github-wills.12zz7@aleeas.com>
2023-06-13 16:43:24 +0100

    Create compose rule extensions for finding nodes using string ids during instrumented tests

---
+++ app/src/androidTest/java/com/willbsp/habits/helper/ComposeRuleExtensions.kt
@@ -0,0 +1,17 @@
+package com.willbsp.habits.helper
+
+import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithTextId(
+    @StringRes id: Int
+): SemanticsNodeInteraction = onNodeWithText(activity.getString(id))
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithContentDescriptionId(
+    @StringRes id: Int
+): SemanticsNodeInteraction = onNodeWithContentDescription(activity.getString(id))
