d8eac8aea6e228f32b56b6f3286c60d0ed31fe1b
willbsp <github-wills.12zz7@aleeas.com>
2023-06-24 10:26:28 +0100

    Add first logbook tests

---
+++ app/src/androidTest/java/com/willbsp/habits/ui/LogbookScreenTest.kt
@@ -0,0 +1,145 @@
+package com.willbsp.habits.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.willbsp.habits.HiltComponentActivity
+import com.willbsp.habits.R
+import com.willbsp.habits.data.TestData.habit1
+import com.willbsp.habits.data.TestData.habit2
+import com.willbsp.habits.data.repository.EntryRepository
+import com.willbsp.habits.data.repository.HabitRepository
+import com.willbsp.habits.domain.usecase.GetHabitsWithVirtualEntriesUseCase
+import com.willbsp.habits.domain.usecase.GetVirtualEntriesUseCase
+import com.willbsp.habits.helper.onNodeWithContentDescriptionId
+import com.willbsp.habits.helper.onNodeWithTextId
+import com.willbsp.habits.ui.screens.logbook.LogbookScreen
+import com.willbsp.habits.ui.screens.logbook.LogbookViewModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.Clock
+import java.time.Instant
+import java.time.LocalDate
+import java.time.Month
+import java.time.ZoneOffset
+import java.time.format.TextStyle
+import java.util.Locale
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class LogbookScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
+
+    private lateinit var activity: ComponentActivity
+
+    @Inject
+    lateinit var habitRepository: HabitRepository
+
+    @Inject
+    lateinit var entryRepository: EntryRepository
+
+    private val date = LocalDate.parse("2023-03-10")
+    private val time = "T12:00:00Z"
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        val getVEntries = GetVirtualEntriesUseCase(habitRepository, entryRepository)
+        val getHabitsWithVEntries = GetHabitsWithVirtualEntriesUseCase(habitRepository, getVEntries)
+        val clock = Clock.fixed(Instant.parse(date.toString() + time), ZoneOffset.UTC)
+        composeTestRule.setContent {
+            val viewModel = LogbookViewModel(
+                habitRepository = habitRepository,
+                entryRepository = entryRepository,
+                getVirtualEntries = getHabitsWithVEntries,
+                clock = clock
+            )
+            Surface {
+                val state by viewModel.uiState.collectAsStateWithLifecycle()
+                LogbookScreen(
+                    navigateUp = { },
+                    logbookUiState = state,
+                    completedOnClick = { viewModel.toggleEntry(it) },
+                    habitOnClick = { viewModel.setSelectedHabit(it) }
+                )
+            }
+            activity = composeTestRule.activity
+        }
+    }
+
+    @Test
+    fun noHabits_noHabitsScreen() {
+        composeTestRule.onNodeWithTextId(R.string.logbook_empty_text).assertExists()
+    }
+
+    @Test
+    fun habitsExist_habitsShown() = runTest {
+        habitRepository.upsertHabit(habit1)
+        habitRepository.upsertHabit(habit2)
+        composeTestRule.onNodeWithText(habit1.name).assertExists()
+        composeTestRule.onNodeWithText(habit2.name).assertExists()
+    }
+
+    @Test
+    fun calendarMonths_areCorrect() = runTest {
+        habitRepository.upsertHabit(habit1)
+        composeTestRule.onNodeWithText(
+            date.month.getDisplayName(TextStyle.FULL, Locale.getDefault()), substring = true
+        ).assertExists()
+        previousMonth(date.month)
+        composeTestRule.onNodeWithText(
+            date.minusMonths(1).month
+                .getDisplayName(TextStyle.FULL, Locale.getDefault()), substring = true
+        ).assertExists()
+    }
+
+    @Test
+    fun calendarDays_areCorrect() = runTest {
+        habitRepository.upsertHabit(habit1)
+        composeTestRule.onNodeWithText(date.month.maxLength().toString()).assertExists()
+        previousMonth(date.month)
+        previousMonth(date.month.minus(1))
+        composeTestRule.onNodeWithText(date.minusMonths(2).month.maxLength().toString())
+            .assertExists()
+        previousMonth(date.month.minus(2))
+        composeTestRule.onNodeWithText(date.minusMonths(3).month.maxLength().toString())
+            .assertExists()
+    }
+
+    private fun nextMonth(currentMonth: Month) {
+        composeTestRule.onNodeWithContentDescriptionId(R.string.logbook_next_month)
+            .performClick()
+    }
+
+    private fun previousMonth(currentMonth: Month) {
+        composeTestRule.onNodeWithTag(
+            activity.getString(R.string.logbook_previous_month) + " " + currentMonth.getDisplayName(
+                TextStyle.FULL,
+                Locale.getDefault()
+            ) + " " + date.year
+        ).performClick()
+        composeTestRule.onNodeWithText(
+            "${date.month.getDisplayName(TextStyle.FULL, Locale.getDefault())} ${date.year}"
+        )
+    }
+
+}
