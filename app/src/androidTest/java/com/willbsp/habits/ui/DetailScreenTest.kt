9a8346f42490d45174d4034cb8cc9fcd5921b650
willbsp <github-wills.12zz7@aleeas.com>
2023-06-25 11:12:53 +0100

    Create tests for detail screen

---
+++ app/src/androidTest/java/com/willbsp/habits/ui/DetailScreenTest.kt
@@ -0,0 +1,182 @@
+package com.willbsp.habits.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onSiblings
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.willbsp.habits.HiltComponentActivity
+import com.willbsp.habits.R
+import com.willbsp.habits.data.TestData.habit1
+import com.willbsp.habits.data.TestData.habit2
+import com.willbsp.habits.data.TestData.habit3
+import com.willbsp.habits.data.TestData.habit4
+import com.willbsp.habits.data.model.Habit
+import com.willbsp.habits.data.repository.EntryRepository
+import com.willbsp.habits.data.repository.HabitRepository
+import com.willbsp.habits.domain.usecase.CalculateScoreUseCase
+import com.willbsp.habits.domain.usecase.CalculateStatisticsUseCase
+import com.willbsp.habits.domain.usecase.CalculateStreakUseCase
+import com.willbsp.habits.domain.usecase.GetVirtualEntriesUseCase
+import com.willbsp.habits.fake.repository.FakeEntryRepository
+import com.willbsp.habits.helper.onNodeWithTextId
+import com.willbsp.habits.ui.screens.detail.DetailScreen
+import com.willbsp.habits.ui.screens.detail.DetailViewModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.Clock
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.time.format.TextStyle
+import java.util.Locale
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class DetailScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
+
+    @Inject
+    lateinit var habitRepository: HabitRepository
+
+    @Inject
+    lateinit var entryRepository: EntryRepository
+
+    private lateinit var activity: ComponentActivity
+
+    private lateinit var viewModel: DetailViewModel
+    private lateinit var clock: Clock
+    private lateinit var getVirtualEntriesUseCase: GetVirtualEntriesUseCase
+    private lateinit var calculateScoreUseCase: CalculateScoreUseCase
+    private lateinit var calculateStreakUseCase: CalculateStreakUseCase
+    private lateinit var calculateStatisticsUseCase: CalculateStatisticsUseCase
+
+    private val date = LocalDate.parse("2023-03-10")
+    private val time = "T12:00:00Z"
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        runBlocking { habitRepository.upsertHabit(habit1) }
+        clock = Clock.fixed(Instant.parse(date.toString() + time), ZoneOffset.UTC)
+        getVirtualEntriesUseCase = GetVirtualEntriesUseCase(habitRepository, entryRepository)
+        calculateScoreUseCase = CalculateScoreUseCase(getVirtualEntriesUseCase, clock)
+        calculateStreakUseCase = CalculateStreakUseCase(getVirtualEntriesUseCase, clock)
+        calculateStatisticsUseCase = CalculateStatisticsUseCase(entryRepository)
+    }
+
+    private fun init(habit: Habit) {
+        runBlocking { habitRepository.upsertHabit(habit) }
+        composeTestRule.setContent {
+            viewModel = DetailViewModel(
+                habitRepository = habitRepository,
+                savedStateHandle = SavedStateHandle(mapOf(Pair("habitId", habit.id))),
+                calculateScoreUseCase = calculateScoreUseCase,
+                calculateStreakUseCase = calculateStreakUseCase,
+                calculateStatisticsUseCase = calculateStatisticsUseCase,
+                clock = clock
+            )
+            Surface {
+                val state by viewModel.uiState.collectAsStateWithLifecycle()
+                DetailScreen(
+                    detailUiState = state,
+                    navigateUp = { },
+                    navigateToEditHabit = { }
+                )
+            }
+        }
+        activity = composeTestRule.activity
+    }
+
+    @Test
+    fun dailyHabit_showsCorrectFrequency() {
+        init(habit1)
+        composeTestRule.onNodeWithTextId(R.string.detail_every_day).assertExists()
+    }
+
+    @Test
+    fun weeklyHabitOnceAWeek_showsCorrectFrequency() {
+        init(habit2)
+        composeTestRule.onNodeWithText(
+            activity.resources.getQuantityString(R.plurals.detail_times_per_week, 1, 1)
+        ).assertExists()
+    }
+
+    @Test
+    fun weeklyHabitMultipleTimesWeek_showsCorrectFrequency() {
+        init(habit4)
+        composeTestRule.onNodeWithText(
+            activity.resources.getQuantityString(R.plurals.detail_times_per_week, 3, 3)
+        ).assertExists()
+    }
+
+    @Test
+    fun dailyHabit_showsCorrectScore() = runTest {
+        init(habit3)
+        (entryRepository as FakeEntryRepository).populate()
+        composeTestRule.onNodeWithText("37%").assertExists()
+    }
+
+    @Test
+    fun dailyHabit_showsCorrectStreak() = runTest {
+        init(habit3)
+        (entryRepository as FakeEntryRepository).populate()
+        composeTestRule.onNodeWithTextId(R.string.detail_streak)
+            .onSiblings().filterToOne(hasText("5")).assertExists()
+    }
+
+    @Test
+    fun dailyHabit_showsLongestStreak() = runTest {
+        init(habit3)
+        (entryRepository as FakeEntryRepository).populate()
+        composeTestRule.onNodeWithTextId(R.string.detail_longest_streak)
+            .onSiblings().filterToOne(hasText("5")).assertExists()
+    }
+
+    @Test
+    fun dailyHabit_showsCorrectTotal() = runTest {
+        init(habit3)
+        (entryRepository as FakeEntryRepository).populate()
+        composeTestRule.onNodeWithTextId(R.string.detail_total)
+            .onSiblings().filterToOne(hasText("11")).assertExists()
+    }
+
+    @Test
+    fun dailyHabit_showsCorrectStartDate() = runTest {
+        init(habit3)
+        val startDate = LocalDate.parse("2023-02-11")
+        (entryRepository as FakeEntryRepository).populate()
+        composeTestRule.onNodeWithTextId(R.string.detail_started)
+            .onSiblings().filterToOne(
+                hasText(
+                    "${startDate.dayOfMonth} ${
+                        startDate.month.getDisplayName(
+                            TextStyle.SHORT,
+                            Locale.getDefault()
+                        )
+                    }"
+                )
+            ).assertExists()
+    }
+
+}
