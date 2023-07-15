3e321a4e83254208acc7933a4574768ebae4f4bc
willbsp <github-wills.12zz7@aleeas.com>
2023-07-15 11:28:35 +0100

    Create ExportDatabaseUseCase to handle exporting database to selected file

---
+++ app/src/main/java/com/willbsp/habits/domain/usecase/ExportDatabaseUseCase.kt
@@ -0,0 +1,37 @@
+package com.willbsp.habits.domain.usecase
+
+import android.content.Context
+import android.net.Uri
+import androidx.sqlite.db.SimpleSQLiteQuery
+import com.willbsp.habits.common.DATABASE_NAME
+import com.willbsp.habits.data.database.dao.RawDao
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class ExportDatabaseUseCase @Inject constructor(
+    @ApplicationContext val context: Context,
+    private val rawDao: RawDao
+) {
+
+    // Query ensures that a checkpoint is created, so write ahead log is written to main db file
+    private val checkpointQuery = SimpleSQLiteQuery("pragma wal_checkpoint(full)")
+
+    suspend operator fun invoke(destinationUri: Uri) = withContext(Dispatchers.IO) {
+
+        val database = context.getDatabasePath(DATABASE_NAME)
+        val contentResolver = context.contentResolver
+
+        if (database.exists()) {
+            rawDao.rawQuery(checkpointQuery)
+            val output = contentResolver.openOutputStream(destinationUri)
+            if (output != null) {
+                output.write(database.readBytes())
+                output.close()
+            }
+        }
+
+    }
+
+}
