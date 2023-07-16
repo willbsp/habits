545cc7e90c88e6dd51d0f551430d2b91404f9453
willbsp <github-wills.12zz7@aleeas.com>
2023-07-16 18:35:28 +0100

    Update ImportDatabaseUseCase to check for validity of database before import and restore the old if invalid

--- app/src/main/java/com/willbsp/habits/domain/usecase/ImportDatabaseUseCase.kt
+++ app/src/main/java/com/willbsp/habits/domain/usecase/ImportDatabaseUseCase.kt
@@ -2,8 +2,10 @@ package com.willbsp.habits.domain.usecase
 
 import android.content.Context
 import android.net.Uri
+import androidx.room.Room
 import com.willbsp.habits.common.DATABASE_NAME
 import com.willbsp.habits.data.database.HabitDatabase
+import com.willbsp.habits.data.database.MIGRATION_1_2
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.io.File
 import javax.inject.Inject
@@ -13,32 +15,53 @@ class ImportDatabaseUseCase @Inject constructor(
     private var habitDatabase: HabitDatabase,
 ) {
 
-    operator fun invoke(sourceUri: Uri) {
+    // TODO move off the main thread
+    operator fun invoke(sourceUri: Uri): Boolean? {
 
-        val database = context.getDatabasePath(DATABASE_NAME)
+        val databaseFile = context.getDatabasePath(DATABASE_NAME)
         val contentResolver = context.contentResolver
 
-        if (database.exists()) {
+        if (databaseFile.exists()) {
             val input = contentResolver.openInputStream(sourceUri)
             if (input != null) {
 
+                val oldDatabaseFile = File.createTempFile("old", ".db")
                 habitDatabase.close()
                 if (!habitDatabase.isOpen) {
-                    val databaseDirectory = File(database.parent!!)
-                    if (databaseDirectory.isDirectory) {
-                        for (child: File in databaseDirectory.listFiles()!!) {
-                            child.deleteRecursively()
-                        }
-                    }
-                    database.writeBytes(input.readBytes())
+                    oldDatabaseFile.writeBytes(databaseFile.readBytes())
+                    deleteDatabaseFiles(databaseFile)
+                    databaseFile.writeBytes(input.readBytes())
+                }
+
+                try {
+                    Room.databaseBuilder(context, HabitDatabase::class.java, DATABASE_NAME)
+                        .addMigrations(MIGRATION_1_2)
+                        .build().openHelper.writableDatabase
+                } catch (e: Exception) {
+                    deleteDatabaseFiles(databaseFile)
+                    databaseFile.writeBytes(oldDatabaseFile.readBytes())
+                    input.close()
+                    return false
                 }
 
                 input.close()
+                return true
 
             }
 
         }
 
+        return null
+
+    }
+
+    private fun deleteDatabaseFiles(databaseFile: File) {
+        val databaseDirectory = File(databaseFile.parent!!)
+        if (databaseDirectory.isDirectory) {
+            for (child: File in databaseDirectory.listFiles()!!) {
+                child.deleteRecursively()
+            }
+        }
     }
 
 }
