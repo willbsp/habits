5bb19b52df8a7dca6b78b8c35da6e147d5459615
willbsp <github-wills.12zz7@aleeas.com>
2023-07-18 17:04:35 +0100

    Add methods for validating the database and getting the database path to LocalDatabaseUtils

--- app/src/main/java/com/willbsp/habits/data/database/util/DatabaseUtils.kt
+++ app/src/main/java/com/willbsp/habits/data/database/util/DatabaseUtils.kt
@@ -1,6 +1,12 @@
 package com.willbsp.habits.data.database.util
 
+import java.io.File
+
 interface DatabaseUtils {
+
+    fun validateDatabase(): Boolean
+
+    fun getDatabasePath(): File
 
     fun closeDatabase()
 
