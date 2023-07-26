e5161cb90c6f6107f81a1acbf60beab57a52dec1
willbsp <github-wills.12zz7@aleeas.com>
2023-07-26 20:29:43 +0100

    Create TimePickerDialog

---
+++ app/src/main/java/com/willbsp/habits/ui/common/TimePickerDialog.kt
@@ -0,0 +1,61 @@
+package com.willbsp.habits.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.willbsp.habits.R
+
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@Composable
+fun TimePickerDialog(
+    modifier: Modifier = Modifier,
+    state: TimePickerState,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+) {
+
+    AlertDialog(onDismissRequest = onCancel) {
+        Surface(
+            modifier = modifier,
+            shape = AlertDialogDefaults.shape,
+            color = AlertDialogDefaults.containerColor,
+            tonalElevation = AlertDialogDefaults.TonalElevation
+        ) {
+            Column(modifier = modifier.padding(15.dp)) {
+                TimePicker(state = state)
+                FlowRow(
+                    modifier = modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    TextButton(onClick = onCancel) {
+                        Text(stringResource(R.string.timepicker_dismiss))
+                    }
+                    Button(onClick = onConfirm) {
+                        Text(stringResource(R.string.timepicker_confirm))
+                    }
+                }
+            }
+        }
+
+    }
+
+}
