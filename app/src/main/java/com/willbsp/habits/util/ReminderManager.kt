63cb06ef72b769fadd16236608e45872ec7ca060
willbsp <github-wills.12zz7@aleeas.com>
2023-08-02 07:56:13 +0100

    Set up ReminderManager and ReminderReceiver for scheduling reminders

---
+++ app/src/main/java/com/willbsp/habits/util/ReminderManager.kt
@@ -0,0 +1,71 @@
+package com.willbsp.habits.util
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.time.DayOfWeek
+import java.time.LocalTime
+import java.util.Calendar
+import java.util.Locale
+import javax.inject.Inject
+
+class ReminderManager @Inject constructor(
+    @ApplicationContext val context: Context
+) {
+
+    fun scheduleReminder(
+        day: DayOfWeek,
+        time: LocalTime,
+        reminderId: Int
+    ) {
+
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (!alarmManager.canScheduleExactAlarms()) return
+        }
+
+        val intent =
+            Intent(context.applicationContext, ReminderReceiver::class.java).let { intent ->
+                PendingIntent.getBroadcast(
+                    context.applicationContext, reminderId, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+
+        val reminderTime: Calendar = Calendar.getInstance(Locale.getDefault()).apply {
+            set(Calendar.HOUR_OF_DAY, time.hour)
+            set(Calendar.MINUTE, time.minute)
+            set(Calendar.DAY_OF_WEEK, day.toCalendarDay())
+        }
+
+        alarmManager.setAlarmClock(
+            AlarmManager.AlarmClockInfo(reminderTime.timeInMillis, intent),
+            intent
+        )
+
+    }
+
+    fun unscheduleReminder(
+        reminderId: Int
+    ) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, ReminderReceiver::class.java).let { intent ->
+            PendingIntent.getBroadcast(
+                context,
+                reminderId,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+        alarmManager.cancel(intent)
+    }
+
+    private fun DayOfWeek.toCalendarDay(): Int {
+        return (value % 7) + 1
+    }
+
+}
---
+++ app/src/main/java/com/willbsp/habits/util/ReminderReceiver.kt
@@ -0,0 +1,39 @@
+package com.willbsp.habits.util
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.willbsp.habits.common.REMINDER_NOTIFICATION_CHANNEL_ID
+
+class ReminderReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManager.sendReminderNotification(
+            applicationContext = context,
+            channelId = REMINDER_NOTIFICATION_CHANNEL_ID
+        )
+
+    }
+
+    private fun NotificationManager.sendReminderNotification(
+        applicationContext: Context,
+        channelId: String
+    ) {
+        val builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setContentTitle("title text")
+            .setContentText("body text")
+            .setSmallIcon(androidx.core.R.drawable.ic_call_answer)
+            .setAutoCancel(true)
+        notify(1, builder.build())
+    }
+
+}
