5a0834b280afcbafcda25bf2e34fc0e36bb0231f
willbsp <github-wills.12zz7@aleeas.com>
2023-08-19 15:26:00 +0100

    Modify notification to start the app when clicked

--- app/src/main/java/com/willbsp/habits/util/ReminderReceiver.kt
+++ app/src/main/java/com/willbsp/habits/util/ReminderReceiver.kt
@@ -1,11 +1,13 @@
 package com.willbsp.habits.util
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import com.willbsp.habits.MainActivity
 import com.willbsp.habits.R
 import com.willbsp.habits.common.REMINDER_NOTIFICATION_CHANNEL_ID
 import com.willbsp.habits.data.repository.EntryRepository
@@ -63,15 +65,23 @@ class ReminderReceiver : BroadcastReceiver() {
     }
 
     private fun NotificationManager.sendReminderNotification(
-        applicationContext: Context,
+        context: Context,
         reminderId: Int,
         habitName: String
     ) {
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         val builder =
-            NotificationCompat.Builder(applicationContext, REMINDER_NOTIFICATION_CHANNEL_ID)
+            NotificationCompat.Builder(context, REMINDER_NOTIFICATION_CHANNEL_ID)
                 .setContentTitle(habitName)
-                .setContentText("Have you completed your habit today?")
+                .setContentText("Did you complete your habit today?")
                 .setSmallIcon(R.drawable.ic_notification)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
         notify(reminderId, builder.build())
     }
