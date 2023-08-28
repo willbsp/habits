b220ce3316d1089b91d402ab59f227c8f23fe535
willbsp <github-wills.12zz7@aleeas.com>
2023-08-28 17:15:51 +0100

    Fix FakeReminderRepository method return type

--- shared-test/src/main/java/com/willbsp/habits/fake/repository/FakeReminderRepository.kt
+++ shared-test/src/main/java/com/willbsp/habits/fake/repository/FakeReminderRepository.kt
@@ -21,8 +21,8 @@ class FakeReminderRepository : ReminderRepository {
     override fun getRemindersForDayStream(day: DayOfWeek): Flow<List<Reminder>> =
         observableReminders.map { reminders -> reminders.filter { it.day == day } }
 
-    override fun getReminderStream(reminderId: Int): Flow<Reminder> =
-        observableReminders.map { reminders -> reminders.first { it.id == reminderId } }
+    override fun getReminderStream(reminderId: Int): Flow<Reminder?> =
+        observableReminders.map { reminders -> reminders.find { it.id == reminderId } }
 
     override suspend fun insertReminder(reminder: Reminder) {
         reminders.add(reminder)
