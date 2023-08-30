a8d23a26952bec6675211ba58baf75c4d3875046
willbsp <github-wills.12zz7@aleeas.com>
2023-08-30 20:43:50 +0100

    Add string to strings.xml

--- app/src/main/java/com/willbsp/habits/ui/common/form/HabitForm.kt
+++ app/src/main/java/com/willbsp/habits/ui/common/form/HabitForm.kt
@@ -14,8 +14,19 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccessAlarm
 import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -276,7 +287,7 @@ private fun HabitReminderDayField(
         readOnly = true,
         isError = isInvalid,
         onValueChange = {},
-        label = { Text("Days") },
+        label = { Text(stringResource(R.string.modify_reminder_days)) },
         interactionSource = source,
         trailingIcon = {
             Icon(
